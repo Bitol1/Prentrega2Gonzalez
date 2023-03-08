bfc3e3014616e6e240e202efce263741d55e1c9b
GPabloMatias <gpablomatias1@gmail.com>
2023-03-08 03:30:47 -0300

    Commit correspondiente a la segunda pre entrega, simulador con arrays, objetos y búsquedas dentro del array

---
+++ utils.js
@@ -0,0 +1,56 @@
+const mostrarMensaje = (mensaje) => alert(mensaje);
+
+const cuotasArray = [{
+        3: 1.101
+    },
+    {
+        6: 2.078
+    },
+    {
+        10: 2.669
+    },
+    {
+        12: 4.506
+    },
+    {
+        18: 7.379
+    },
+    {
+        24: 10.778
+    },
+];
+
+class Financiacion {
+    constructor(monto, cuotas) {
+        this.monto = monto;
+        this.cuotas = cuotas;
+        this.porcentajes = cuotasArray.find(cuota => cuota[cuotas]);
+    }
+
+    calcularCuota() {
+        if (!this.porcentajes) {
+            mostrarMensaje("Cantidad de cuotas no válida");
+            return;
+        }
+
+        const porcentaje = this.porcentajes[this.cuotas];
+        const montoCuota = this.monto * porcentaje / this.cuotas;
+        mostrarMensaje(`El monto a pagar por cuota es de $${montoCuota.toFixed(2)}.`);
+    }
+}
+
+let continuar = true;
+do {
+    const monto = prompt("Ingrese el monto del producto:")
+    if (monto === null) {
+        break;
+    } else if (isNaN(monto) || monto <= 0) {
+        mostrarMensaje("El monto ingresado no es válido");
+    } else if (monto > 0) {
+        const cuotas = Number(prompt("Ingrese la cantidad de cuotas en un rango de 3, 6, 10, 12, 18 y 24 cuotas:"));
+        const financiacion = new Financiacion(monto, cuotas);
+        financiacion.calcularCuota();
+    } 
+    continuar = confirm("¿Desea realizar otro cálculo?");
+    
+} while (continuar);
